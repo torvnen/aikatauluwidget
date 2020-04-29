c910269dde6e1abea0ff87152fae15dd54595084
Niko Torvinen <torvnen@gmail.com>
2020-04-29 13:45:30 +0300

    Open configuration activity on widget click

--- app/src/main/java/com/example/aikataulu/WidgetProvider.kt
+++ app/src/main/java/com/example/aikataulu/WidgetProvider.kt
@@ -1,10 +1,14 @@
 package com.example.aikataulu;
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.RemoteViews
+import com.example.aikataulu.ui.main.MainActivity
+
 
 class WidgetProvider : AppWidgetProvider() {
     private val TAG = "TIMETABLE.WidgetProvider"
@@ -17,6 +21,18 @@ class WidgetProvider : AppWidgetProvider() {
         // Start Service (don't worry about repeating, multiple calls will be handled by the system)
         Log.i(TAG, "Invoking service startup")
         context.startForegroundService(Intent(context, TimetableService::class.java))
+
+        // Attach click handler to open configuration
+        appWidgetIds.forEach { widgetId ->
+            val intent = Intent(context, MainActivity::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+            val views = RemoteViews(context.packageName, R.layout.widget)
+            views.setOnClickPendingIntent(R.id.widgetContainer, pendingIntent)
+
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
     }
 
     override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
