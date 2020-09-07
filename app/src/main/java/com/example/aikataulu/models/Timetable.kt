860530d23f9166d9de360c9b8a18757ba379288e
Niko Torvinen <torvnen@gmail.com>
2020-09-07 12:02:47 +0300

    Add new columns IS_VIEW_UPDATED and WIDGET_ID

--- app/src/main/java/com/example/aikataulu/models/Timetable.kt
+++ app/src/main/java/com/example/aikataulu/models/Timetable.kt
@@ -3,6 +3,7 @@ package com.example.aikataulu.models
 import android.database.MatrixCursor
 
 class Timetable(val widgetId: Int, val stop: Stop, val departures: List<Departure>) {
+    var isViewUpdated: Boolean = false
     companion object {
         const val COLUMN_STOPNAME = "STOPNAME"
         const val COLUMN_DEPARTURE_SCHEDULED = "DEPARTURE_SCHEDULED"
@@ -10,13 +11,17 @@ class Timetable(val widgetId: Int, val stop: Stop, val departures: List<Departur
         const val COLUMN_IS_ON_TIME = "IS_ON_TIME"
         const val COLUMN_HEADSIGN = "HEADSIGN"
         const val COLUMN_ROUTE_SHORT_NAME = "ROUTE_SHORT_NAME"
+        const val IS_VIEW_UPDATED = "VIEW_UPDATED"
+        const val WIDGET_ID = "WIDGET_ID"
         val allColumns = arrayOf(
             COLUMN_STOPNAME,
             COLUMN_ROUTE_SHORT_NAME,
             COLUMN_HEADSIGN,
             COLUMN_DEPARTURE_SCHEDULED,
             COLUMN_DEPARTURE_REALTIME,
-            COLUMN_IS_ON_TIME
+            COLUMN_IS_ON_TIME,
+            IS_VIEW_UPDATED,
+            WIDGET_ID
         )
     }
 
@@ -34,7 +39,9 @@ class Timetable(val widgetId: Int, val stop: Stop, val departures: List<Departur
                 d.headsign,
                 d.scheduledDeparture,
                 d.realtimeDeparture,
-                d.isOnTime
+                d.isOnTime,
+                 isViewUpdated,
+                 widgetId
             )
         }
     }
