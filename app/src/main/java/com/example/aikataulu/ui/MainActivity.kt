d57b47ed6d95a67c43c0288b56736aa10fc21072
Niko Torvinen <torvnen@gmail.com>
2020-09-07 12:39:54 +0300

    Do not initiate notification channel in onCreate - NC consumer should check for initiation.

--- app/src/main/java/com/example/aikataulu/ui/MainActivity.kt
+++ app/src/main/java/com/example/aikataulu/ui/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.aikataulu.ui
 
-import android.app.*
-import android.content.Context
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import com.example.aikataulu.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aikataulu.R
+import com.example.aikataulu.TimetableService
+import com.example.aikataulu.TimetableWidgetProvider
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -18,20 +22,23 @@ class MainActivity : AppCompatActivity() {
         var notificationChannelInitiated = false
 
         fun notificationChannelId(notificationManager: NotificationManager): String {
-            if (!notificationChannelInitiated) {
-                Log.i("TIMETABLE", "Initiating notification channel...")
-                // Create the NotificationChannel
-                val channel = NotificationChannel(
-                    NOTIFICATION_CHANNEL_ID,
-                    NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_NONE)
-                channel.description = NOTIFICATION_CHANNEL_DESC
-                channel.lightColor = Color.MAGENTA
-                channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-                // Register the channel with the system; you can't change the importance
-                // or other notification behaviors after this
-                notificationManager.createNotificationChannel(channel)
-                notificationChannelInitiated =  true
-                Log.i("TIMETABLE", "Notification channel initiated.")
+            if (!notificationChannelInitiated){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // API Level 26+ requires a registered notification channel.
+                    Log.i("TIMETABLE", "Initiating notification channel...")
+                    // Create the NotificationChannel
+                    val channel = NotificationChannel(
+                        NOTIFICATION_CHANNEL_ID,
+                        NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_NONE)
+                    channel.description = NOTIFICATION_CHANNEL_DESC
+                    channel.lightColor = Color.MAGENTA
+                    channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+                    // Register the channel with the system; you can't change the importance
+                    // or other notification behaviors after this
+                    notificationManager.createNotificationChannel(channel)
+                    notificationChannelInitiated =  true
+                    Log.i("TIMETABLE", "Notification channel initiated.")
+                } else Log.i("TIMETABLE", "API Version is lower than 26. Not initiating notification channel.")
             }
             return NOTIFICATION_CHANNEL_ID
         }
@@ -43,14 +50,17 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "Exit flag was set - finishing activity.")
             finish()
         }
+        // Set the default view when the app is opened from the launcher
+        // TODO have the main activity content view fetch all existing widgets and provide buttons for configuring them?
         setContentView(R.layout.main_activity)
-        if (!notificationChannelInitiated)
-            notificationChannelId(
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            )
+
         val widgetIds = TimetableWidgetProvider.getExistingWidgetIds(applicationContext)
         if (widgetIds.isNotEmpty()) {
-            applicationContext.startForegroundService(Intent(applicationContext, TimetableService::class.java))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                applicationContext.startForegroundService(Intent(applicationContext, TimetableService::class.java))
+            } else {
+                applicationContext.startService(Intent(applicationContext, TimetableService::class.java))
+            }
             widgetIds.forEach { TimetableWidgetProvider.sendUpdateWidgetBroadcast(applicationContext, it) }
         }
 
